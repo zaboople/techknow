852b90061a6fbd80b74acbd6e6f1bcc0be89b903
zaboople <troymotte@yahoo.com>
2017-10-08 12:42:03 -0500

    Should not be there

--- golang/run.sh
+++ golang/run.sh
@@ -1,2 +1,3 @@
+# Note: This works fine under cygwin until you use dashes
 cd $(dirname $0) || exit
 ./build.sh "$@" && ./bin/"$@" "$@"
--- golang/src/github.com/zaboople/interface/interface.go
+++ golang/src/github.com/zaboople/interface/interface.go
@@ -1,45 +1,13 @@
 package main
 
+
 import (
-	"fmt"; "math"
+	"fmt"
 )
 
-// This is our interface:
-type FlatFloat interface {Flat() float64}
-
-// This implements our interface thanks to "structural typing" which is a lot like duck typing:
-type MyFloat float64
-func (f MyFloat) Flat() float64 {
-	if f < 0 {return float64(-f)}
-	return float64(f)
-}
-
-// This also implements FlatFloat, but we've added "Stringer", via String(). 
-// Note that for Stringer, we didn't use a pointer, because with a pointer, it only works for 
-// pointer types; without, it works for either. And since you can't have two functions named String(),
-// we went with the one that works in all cases:
-type Vertex struct {X, Y float64}
-func (v *Vertex) Flat() float64  {return math.Sqrt(v.X*v.X + v.Y*v.Y)}
-func (v *Vertex) String() string {return fmt.Sprintf("<Vertex X=%f Y=%f>", v.X, v.Y)}
 
 func main() {
-	var a FlatFloat
+	fmt.Println("SHIT")
 
-	f := MyFloat(-math.Sqrt2)
-	var g MyFloat=-math.Sqrt2
-	a = f  
-	fmt.Println(a, f, g)
-	
-	
-	// Plain a=v will fail because Flat is defined on a pointer. 
-	// See with interfaces, the coercion back and forth between pointer & not-pointer
-	// just doesn't happen.
-	v := Vertex{3, 4}
-	a = &v 
-	fmt.Println(v.Flat(), a.Flat())
-	
-	//And now back to our Stringer interface. Notice how it only works with a, not v, because
-	//we defined String() on a pointer. No casting happens.
-	fmt.Println(v, a)
 }
 
