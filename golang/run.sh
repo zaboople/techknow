888973c5225412b943c9820a44e31cffb0592e16
zaboople <troymotte@yahoo.com>
2017-10-08 13:09:22 -0500

    Comments

--- golang/run.sh
+++ golang/run.sh
@@ -1,3 +1,7 @@
-# Note: This works fine under cygwin until you use dashes
+# Note: This may not work under cygwin or windows or unix or whatever because
+# 1. flag.Arg(0) is different depending on environment; on cygwin, at least, it's the name of the program.
+#    Probably on Unix/Linux/Gnu too.
+# 2. Other weirdness
+
 cd $(dirname $0) || exit
 ./build.sh "$@" && ./bin/"$@" "$@"
