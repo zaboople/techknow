f6eede415b5c746c453149f0f3d9c6f8e2d646b3
Troy Motte <troy.motte@siemens.com>
2020-11-09 11:42:01 -0600

    java formats

--- java/format.groovy
+++ java/format.groovy
@@ -1,5 +1,12 @@
-// Fixed width decimal:
+println("=========")
+
+println("\n*** Fixed width decimal, right justified:")
 println(String.format("%10.4f", 100D/2234D))
 
-// Left justify:
-println(String.format("%-5d", 111));
+println("\n*** Only care about getting 4 chars after decimal point:")
+println(String.format("%.4f", 100D/34D))
+println(String.format("%.4f", 1001111D/34D))
+println(String.format("%.4f", 1D/34D))
+
+println("\n*** Left justify:")
+println(String.format("%-15d", 111)+"hey");
