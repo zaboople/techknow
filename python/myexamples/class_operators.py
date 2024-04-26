694bfb1a46a537e51c056e35eb676d2886827f6e
zaboople <troy.motte@gmail.com>
2024-04-25 19:53:56 -0500

    python

--- python/myexamples/class_operators.py
+++ python/myexamples/class_operators.py
@@ -23,19 +23,31 @@ class Pair:
         else:
             return False
 
+    def __lt__(self, other):
+        # Supposedly if I just do this and __eq__, all
+        # the other comparisons fall into place.
+        if isinstance(other, self.__class__):
+            return (self.x < other.x) or (self.x == other.x and self.y < other.y)
+        else:
+            return False
+
     def __hash__(self):
+        # Necessary for sets & maps
         return hash((self.x, self.y))
 
     def __str__(self):
+        # For toString()ing
         return f"Pair({self.x}, {self.y})"
 
     def __repr__(self):
+        # For the repr() global function
         return f"Pair({repr(self.x)}, {repr(self.y)})"
 
 
 p1 = Pair(1, 2)
 p2 = Pair(1, 2)
 p3 = Pair(1, 3)
+p4 = Pair(1, 4)
 print(f"{(p1 is p1) =}")
 print(f"{(p1 is not p1) =}")
 print()
@@ -48,10 +60,17 @@ print(f"{(p1 != p3) =}")
 print(f"{(p1 is not p3) =}")
 
 comment("""
-    Make sure a set works right
+    Make sure a set or dictionary works right
 """)
 myset = {p1,p2,p3,p1}
 print(f"{myset=}")
 myhash = {p1: p2, p2: p3, p3: p1}
 print(f"{myhash=}")
+
+comment("""
+    Can it sort?
+""")
+mylist = [p4, Pair(2, 1), Pair(2, 5), p3, Pair(-1, 1000), p2, p1,]
+for item in sorted([p4, Pair(2, 1), Pair(2, 5), p3, Pair(-1, 1000), p2, p1,]):
+    print(f"  {item}")
 
