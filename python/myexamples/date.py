ba59161efbbaf3515c7b3ba7dec93e3a9338479e
zaboople <troy.motte@gmail.com>
2024-04-30 18:44:00 -0500

    python

--- python/myexamples/date.py
+++ python/myexamples/date.py
@@ -6,8 +6,12 @@ import sys
 
 today = datetime.datetime(year=2017, month=1, day=27)
 print(f"Date: {today}")
+
 today = datetime.datetime.now()
+print("sleeping...")
 sys.stdout.flush()
-time.sleep(2);
+time.sleep(0.1);
 today2 = datetime.datetime.now()
-print((today2 - today).total_seconds())
+print("{0} seconds".format(
+    (today2 - today).total_seconds()
+))
