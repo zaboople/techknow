61a31696c9933e7fc138056c59aa6d877b1bf561
zaboople <troy.motte@gmail.com>
2024-05-23 16:46:38 -0500

    x

--- react/vite-react/src/pages/signup.jsx
+++ react/vite-react/src/pages/signup.jsx
@@ -2,6 +2,31 @@ import {React, useState, useEffect} from "react";
 import Navbar from "../components/Navbar";
 import "./styles.css";
 
+function DotDot() {
+    const [dotCount, setDots] = useState(0);
+    useEffect(()=>{
+        var ok = true;
+        async function doDots() {
+             while (ok) {
+                var i=1;
+                await new Promise(
+                    resolver => setTimeout(()=>resolver(), 200)
+                );
+                if (ok)
+                    setDots(x => (x==3) ?0 :(i=x+1));
+             }
+        }
+        doDots();
+        return ()=>{
+            ok=false;
+            console.log("Stopped dots.");
+        }
+    }, []);
+    var s="";
+    for (var i=0; i<dotCount; i++)
+        s+="."
+    return s;
+}
 function DoSignUp() {
     console.log("SignUp()...");
     const whereAmI = window.location.href;
@@ -13,7 +38,7 @@ function DoSignUp() {
                 ) + "samplejson.json";
             console.log("Calling... "+url);
             await new Promise(
-                resolver => setTimeout(()=>resolver("dook"), 2000)
+                resolver => setTimeout(()=>resolver("dook"), 3000)
             );
             const response = await fetch(url);
             const myjson = await response.json();
@@ -26,7 +51,7 @@ function DoSignUp() {
     }
     useEffect(()=>{initFunc()}, []);
 
-    var dataDisplay = <p>Loading...</p>;
+    var dataDisplay = <p><em>Loading</em><DotDot/></p>;
     if (result.error!=null)
         dataDisplay = <p>Error: {result.error.message}</p>;
     else if (result.data!=null)
@@ -42,7 +67,7 @@ const SignUp = () => {
     return <>
         <Navbar/>
         <div className="subbody">
-            <h2> Signed up </h2>
+            <h2> Who... signed up? </h2>
             These people signed up. <em>You</em> can't:
             <DoSignUp/>
         </div>
